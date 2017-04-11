6356c68711e548e569072890b46ef09f4192d01b
ages944 <ages944@gmail.com>
2017-04-11 01:29:41 -0500

    Additional comments and another constant added

--- gitTestProjectOne/ViewController.swift
+++ gitTestProjectOne/ViewController.swift
@@ -12,16 +12,20 @@ class ViewController: UIViewController {
 
     
     let sampleConstant = 21;
+    let sampleConstantTwo = 22;
+    
     
     override func viewDidLoad() {
         super.viewDidLoad();
         // Do any additional setup after loading the view, typically from a nib.
         // this is a ridiculously simple change
+        // this is another stupid simple change, but it gets the idea across... :)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning();
         // Dispose of any resources that can be recreated.
+        // and yet more really simple changes, just to get a better feel of things.
     }
 
 
