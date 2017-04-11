5fe33ad9bf1786f0bc7fb79cb5a407335dea813a
ages944 <ages944@gmail.com>
2017-04-11 00:56:05 -0500

    Initial Commit

---
+++ gitTestProjectOne/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitTestProjectOne
+//
+//  Created by Adrian Duczynski on 170411.
+//  Copyright © 2017 fabstat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
